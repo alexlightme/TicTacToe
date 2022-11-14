3cd328773a0f59d86e3aeca28c3be2e80133c8c7
Alex <alexanderleonard@msn.com>
2022-11-14 16:07:26 -0500

    Continuing to update the pygame board with my logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+from pygame_board_AL import pygame_display
 import numpy as np
 
 from Engine import *
@@ -14,20 +15,20 @@ random.seed(34567)
 DF = pd.DataFrame(np.zeros((3, 3)).astype(int), columns=['A', 'B', 'C'])
 
 engine = game_engine(DF)
-
-print(engine.board)
-
 user_input =True
 player = 1
-for i in range(200):
-    move = randrange(9)
-    engine.make_move(player, move, user_input=user_input)
-    print(engine.board)
-    engine.check_win(player)
-    engine.check_draw()
-    player = (engine.turn-1) % 2 +1
+#/////////////////////////////////////////////////////////////////////////////
+# OLD DISPLAY AND ENGINE
+# for i in range(200):
+#     move = randrange(9)
+#     engine.make_move(player, move, user_input=user_input)
+#     print(engine.board)
+#     engine.check_win(player)
+#     engine.check_draw()
+#     player = (engine.turn-1) % 2 +1
+#/////////////////////////////////////////////////////////////////////////////
 
-
+pg_display = pygame_display(engine)
 
 # Pygame
 while (True):
@@ -38,11 +39,11 @@ while (True):
             pg.quit()
             sys.exit()
         elif event.type == pg.MOUSEBUTTONDOWN:
-            user_click()
-            if (winner or draw):
-                reset_game()
+            pg_display.user_click()
+            if (pg_display.winner or pg_display.draw):
+                pg_display.reset_game()
     pg.display.update()
-    CLOCK.tick(fps)
+    pg_display.CLOCK.tick(pg_display.fps)
 
 
 
--- pygame_board_AL.py
+++ pygame_board_AL.py
@@ -1,10 +1,10 @@
 
-
 import pygame as pg
 import time
 
+
 class pygame_display():
-    def __init__(self):
+    def __init__(self, engine):
         self.XO = 'x'
 
         # storing the winner's value at
@@ -30,13 +30,13 @@ class pygame_display():
         self.line_color = (0, 0, 0)
 
         # setting up a 3 * 3 board in canvas
-        self.board = [[None] * 3, [None] * 3, [None] * 3]
+        self.board = engine.df
 
         # initializing the pygame window
         pg.init()
 
         # setting fps manually
-        fps = 30
+        self.fps = 30
 
         # this is used to track time
         self.CLOCK = pg.time.Clock()
@@ -59,9 +59,32 @@ class pygame_display():
         self.x_img = pg.transform.scale(self.x_img, (80, 80))
         self.o_img = pg.transform.scale(self.y_img, (80, 80))
 
-        game_initiating_window()
+        self.game_initiating_window()
 
+    def draw_status(self):
+        # getting the global variable draw
+        # into action
+        draw = self.draw
 
+        if self.winner is None:
+            message = self.XO.upper() + "'s Turn"
+        else:
+            message = self.winner.upper() + " won !"
+        if draw:
+            message = "Game Draw !"
+
+        # setting a font object
+        font = pg.font.Font(None, 30)
+
+        # setting the font properties like
+        # color and width of the text
+        text = font.render(message, 1, (255, 255, 255))
+
+        # copy the rendered message onto the board
+        # creating a small block at the bottom of the main display
+        self.screen.fill((0, 0, 0), (0, 400, 500, 100))
+        text_rect = text.get_rect(center=(self.width / 2, 500 - 50))
+        self.screen.blit(text, text_rect)
 
     def game_initiating_window(self):
         # displaying over the screen
@@ -73,39 +96,39 @@ class pygame_display():
         self.screen.fill(self.white)
 
         # drawing vertical lines
-        pg.draw.line(self.screen, line_color, (width / 3, 0), (width / 3, height), 7)
-        pg.draw.line(screen, line_color, (width / 3 * 2, 0), (width / 3 * 2, height), 7)
+        pg.draw.line(self.screen, self.line_color, (self.width / 3, 0), (self.width / 3, self.height), 7)
+        pg.draw.line(self.screen, self.line_color, (self.width / 3 * 2, 0), (self.width / 3 * 2, self.height), 7)
 
         # drawing horizontal lines
-        pg.draw.line(screen, line_color, (0, height / 3), (width, height / 3), 7)
-        pg.draw.line(screen, line_color, (0, height / 3 * 2), (width, height / 3 * 2), 7)
-        draw_status()
+        pg.draw.line(self.screen, self.line_color, (0, self.height / 3), (self.width, self.height / 3), 7)
+        pg.draw.line(self.screen, self.line_color, (0, self.height / 3 * 2), (self.width, self.height / 3 * 2), 7)
+        self.draw_status()
 
     def user_click(self):
         # get coordinates of mouse click
         x, y = pg.mouse.get_pos()
 
         # get column of mouse click (1-3)
-        if (x < width / 3):
+        if (x < self.width / 3):
             col = 1
 
-        elif (x < width / 3 * 2):
+        elif (x < self.width / 3 * 2):
             col = 2
 
-        elif (x < width):
+        elif (x < self.width):
             col = 3
 
         else:
             col = None
 
         # get row of mouse click (1-3)
-        if (y < height / 3):
+        if (y < self.height / 3):
             row = 1
 
-        elif (y < height / 3 * 2):
+        elif (y < self.height / 3 * 2):
             row = 2
 
-        elif (y < height):
+        elif (y < self.height):
             row = 3
 
         else:
@@ -118,6 +141,15 @@ class pygame_display():
             global XO
             drawXO(row, col)
             check_win()
+
+    def reset_game(self):
+        global board, winner, XO, draw
+        time.sleep(3)
+        XO = 'x'
+        draw = False
+        self.game_initiating_window()
+        winner = None
+        board = [[None] * 3, [None] * 3, [None] * 3]
 
 
 
